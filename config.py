a87cc9ebb137be9fc86a3b43c1bae286ddbfd077
petrovk23 <120719173+petrovk23@users.noreply.github.com>
2025-01-02 18:35:04 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py (new file)
+class Config:
+    GAMES = {
+        '6_42': {
+            'name': 'Toto 6/42',
+            'max_number': 42,
+            'db_name': 'toto_6_42_draws.db',
+            'csv_file': 'Toto_6_42_with_first_row.csv'
+        },
+        '6_49': {
+            'name': 'Toto 6/49',
+            'max_number': 49,
+            'db_name': 'toto_6_49_draws.db',
+            'csv_file': 'Toto_6_49_with_first_row.csv'
+        }
+    }
